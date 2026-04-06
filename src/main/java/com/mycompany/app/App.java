7908ad35ca5bb32df48450168ddbb80556bc0d13
agneskng <agnes.kng@gmail.com>
2026-04-06 16:03:22 +0800

    make a change to demo poll scm

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("another change zzz");
+        System.out.println("random change to force a build on Jenkins!!!");
     }
 
     public String getMessage() {
